193347413134139f5f620de334bfd07402418f58
NamitaMohanty <namitamohanty692@gmail.com>
2022-07-29 10:35:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes }from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepComponent } from './step/step.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepComponent,
+    HeaderComponent,
+    MainComponent,
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/step/step.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'step-page',
+  templateUrl: './step.component.html',
+  styleUrls: [ './step.component.css']
+})
+export class StepComponent implements OnInit{
+    ngOnInit(){
+
+    }
+}
